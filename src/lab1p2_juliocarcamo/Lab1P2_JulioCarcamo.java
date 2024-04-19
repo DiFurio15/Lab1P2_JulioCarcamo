87e587175115bec9ecde2e29beb8e39775950aef
DiFurio15 <jucec12@gmail.com>
2024-04-19 15:20:11 -0600

    tercer commit

--- src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
+++ src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
@@ -1,5 +1,6 @@
 package lab1p2_juliocarcamo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -11,28 +12,47 @@ public class Lab1P2_JulioCarcamo {
 
         System.out.println("---Laboratorio 1 de programacion 2--");
 
-        System.out.println("Mediana de medianas ");
-        int TamanioMatriz = Asus.nextInt();
-        if (TamanioMatriz < 4 || (TamanioMatriz % 2) == 0) {
-            int[][] Matriz = new int ([TamanioMatriz][TamanioMatriz]
-        
-    
-    );
+        System.out.println("Ingrese el tamano impar de la matriz mayor que 4:");
+        int tamano = Asus.nextInt();
+
+        // Asegurarse de que el tamaño es impar y mayor que 4
+        if (tamano <= 4 || tamano % 2 == 0) {
+            System.out.println("El tamano debe ser un numero impar mayor que 4.");
+            return;
         }
-    } 
-    
-     private static int[][] generarMatriz(int tamano) {
+
+        // Generar la matriz con numeros aleatorios
+        int[][] matriz = generarMatriz(tamano);
+
+        // Ordenar cada fila de la matriz
+        for (int i = 0; i < matriz.length; i++) {
+            bubbleSortRecursivo(matriz[i], matriz[i].length);
+        }
+
+        // Calcular las medianas de cada fila y almacenarlas
+        ArrayList<Integer> medianas = new ArrayList<>();
+        for (int[] fila : matriz) {
+            medianas.add(calcularMediana(fila));
+        }
+
+        bubbleSortArrayList(medianas);
+
+        int medianaDeMedianas = calcularMediana(medianas.stream().mapToInt(i -> i).toArray());
+        System.out.println("La mediana de medianas es: " + medianaDeMedianas);
+    }
+
+    public static int[][] generarMatriz(int tamano) {
         int[][] matriz = new int[tamano][tamano];
-        Random random = new Random();
+        Random randy = new Random();
         for (int i = 0; i < tamano; i++) {
             for (int j = 0; j < tamano; j++) {
-                matriz[i][j] = random.nextInt(10); // Números entre 0 y 9
+                matriz[i][j] = randy.nextInt(10); // Numeros entre 0 y 9
             }
         }
         return matriz;
     }
 
-    private static void bubbleSort(int[] fila, int n) {
+    public static void bubbleSortRecursivo(int[] fila, int n) {
         if (n == 1) {
             return;
         }
@@ -44,5 +64,27 @@ public class Lab1P2_JulioCarcamo {
             }
         }
 
+    }
+
+    public static int calcularMediana(int[] fila) {
+        int mitad = fila.length / 2;
+        if (fila.length % 2 == 1) {
+            return fila[mitad];
+        } else {
+            return (fila[mitad - 1] + fila[mitad]) / 2;
+        }
+    }
+
+    public static void bubbleSortArrayList(ArrayList<Integer> medianas) {
+        int n = medianas.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (medianas.get(j) > medianas.get(j + 1)) {
+                    int temp = medianas.get(j);
+                    medianas.set(j, medianas.get(j + 1));
+                    medianas.set(j + 1, temp);
+                }
+            }
+        }
     }
 }
