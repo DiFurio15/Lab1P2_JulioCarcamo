3225cc4118b6a2d59a775cff6184d6859792251f
DiFurio15 <jucec12@gmail.com>
2024-04-19 15:44:31 -0600

    cuarto commit

--- src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
+++ src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
@@ -1,6 +1,5 @@
 package lab1p2_juliocarcamo;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -8,83 +7,70 @@ public class Lab1P2_JulioCarcamo {
 
     //Fila 3, asiento 6
     public static void main(String[] args) {
+
         Scanner Asus = new Scanner(System.in);
-
-        System.out.println("---Laboratorio 1 de programacion 2--");
-
         System.out.println("Ingrese el tamano impar de la matriz mayor que 4:");
         int tamano = Asus.nextInt();
 
-        // Asegurarse de que el tamaño es impar y mayor que 4
-        if (tamano <= 4 || tamano % 2 == 0) {
+        // Asegurarse de que el tamano es impar y mayor que 4
+        if (tamano < 4 || tamano % 2 == 0) {
             System.out.println("El tamano debe ser un numero impar mayor que 4.");
             return;
         }
 
         // Generar la matriz con numeros aleatorios
-         int [][] matriz = generarMatriz(tamano);
+        int[][] matriz = generarMatriz(tamano);
 
         // Ordenar cada fila de la matriz
         for (int i = 0; i < matriz.length; i++) {
-            bubbleSortRecursivo(matriz[i], matriz[i].length);
+            bubbleSort(matriz[i]);
         }
 
-        // Calcular las medianas de cada fila y almacenarlas
-        ArrayList<Integer> medianas = new ArrayList<>();
-        for (int[] fila : matriz) {
-            medianas.add(calcularMediana(fila));
+        // Calcular las medianas de cada fila y almacenarlas en una matriz
+        int[] medianas = new int[matriz.length];
+        for (int i = 0; i < matriz.length; i++) {
+            medianas[i] = calcularMediana(matriz[i]);
         }
 
-        bubbleSortArrayList(medianas);
+        // Ordenar las medianas
+        bubbleSort(medianas);
 
-        int medianaDeMedianas = calcularMediana(medianas.stream().mapToInt(i -> i).toArray());
+        // Calcular la mediana de medianas
+        int medianaDeMedianas = calcularMediana(medianas);
         System.out.println("La mediana de medianas es: " + medianaDeMedianas);
     }
 
-    public static int[][] generarMatriz(int tamano) {
+    private static int[][] generarMatriz(int tamano) {
         int[][] matriz = new int[tamano][tamano];
         Random randy = new Random();
         for (int i = 0; i < tamano; i++) {
             for (int j = 0; j < tamano; j++) {
-                matriz[i][j] = randy.nextInt(10); // Numeros entre 0 y 9
+                matriz[i][j] = randy.nextInt(10); // Números entre 0 y 9
             }
+            System.out.println();
         }
         return matriz;
     }
 
-    public static void bubbleSortRecursivo(int[] fila, int n) {
-        if (n == 1) {
-            return;
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (fila[i] > fila[i + 1]) {
-                int temp = fila[i];
-                fila[i] = fila[i + 1];
-                fila[i + 1] = temp;
-            }
-        }
-
-    }
-
-    public static int calcularMediana(int[] fila) {
-        int mitad = fila.length / 2;
-        if (fila.length % 2 == 1) {
-            return fila[mitad];
-        } else {
-            return (fila[mitad - 1] + fila[mitad]) / 2;
-        }
-    }
-
-    public static void bubbleSortArrayList(ArrayList<Integer> medianas) {
-        int n = medianas.size();
+    private static void bubbleSort(int[] arreglo) {
+        int n = arreglo.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (medianas.get(j) > medianas.get(j + 1)) {
-                    int temp = medianas.get(j);
-                    medianas.set(j, medianas.get(j + 1));
-                    medianas.set(j + 1, temp);
+                if (arreglo[j] > arreglo[j + 1]) {
+                    int temp = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temp;
                 }
             }
+        }
+    }
+
+    private static int calcularMediana(int[] arreglo) {
+        int mitad = arreglo.length / 2;
+        if (arreglo.length % 2 == 1) {
+            return arreglo[mitad];
+        } else {
+            return (arreglo[mitad - 1] + arreglo[mitad]) / 2;
         }
     }
 }
