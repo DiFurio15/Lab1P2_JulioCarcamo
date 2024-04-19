d82fba68e9c9613ad961c7ed2a48035bc4d72b89
DiFurio15 <jucec12@gmail.com>
2024-04-19 14:42:08 -0600

    Segundo commit

--- src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
+++ src/lab1p2_juliocarcamo/Lab1P2_JulioCarcamo.java
@@ -1,36 +1,48 @@
 package lab1p2_juliocarcamo;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Lab1P2_JulioCarcamo {
 
+    //Fila 3, asiento 6
     public static void main(String[] args) {
         Scanner Asus = new Scanner(System.in);
 
         System.out.println("---Laboratorio 1 de programacion 2--");
 
-        System.out.println("1.  Mediana de medianas ");
+        System.out.println("Mediana de medianas ");
         int TamanioMatriz = Asus.nextInt();
         if (TamanioMatriz < 4 || (TamanioMatriz % 2) == 0) {
-           int[][] Matriz = new int ([TamanioMatriz][TamanioMatriz]);
+            int[][] Matriz = new int ([TamanioMatriz][TamanioMatriz]
+        
+    
+    );
         }
     } 
     
-     public static void imprimirMatriz(char[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
+     private static int[][] generarMatriz(int tamano) {
+        int[][] matriz = new int[tamano][tamano];
+        Random random = new Random();
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+                matriz[i][j] = random.nextInt(10); // Números entre 0 y 9
             }
-            System.out.println();
         }
+        return matriz;
     }
-     
-     public static void Bubblesort(){
-         for (int i = 0; i < TamanioMatriz; i++) {
-             if (Array[i] > Array[i+1]) {
-                 
-             }
-         }
-     }
 
+    private static void bubbleSort(int[] fila, int n) {
+        if (n == 1) {
+            return;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (fila[i] > fila[i + 1]) {
+                int temp = fila[i];
+                fila[i] = fila[i + 1];
+                fila[i + 1] = temp;
+            }
+        }
+
+    }
 }
